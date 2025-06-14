ca7f9a380853f66d55cfd84eb203f9ae2b726565
Madhusudhankarn <161554056+Madhusudhankarn@users.noreply.github.com>
2025-06-14 14:38:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// Add dynamic year to footer
-document.addEventListener("DOMContentLoaded", () => {
-    const footer = document.querySelector("footer p");
-    const year = new Date().getFullYear();
-    footer.innerHTML = `© ${year} Madhusudhan Reddy | Portfolio Resume`;
-});
+
